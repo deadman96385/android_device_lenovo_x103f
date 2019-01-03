2c5263d22b74a1c360d994982893b6c7ae93c547
deadman96385 <seanhoyt963@gmail.com>
2019-01-03 12:50:02 -0600

    Cleanup Boardconfig
    Remove lenovo stuff, organize into the more standard layout

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,16 +12,51 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# config.mk
-#
-# Product-specific compile-time definitions.
-#
 DEVICE_PATH := device/lenovo/x103f
 
-ifeq ($(TARGET_ARCH),)
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := msm8909
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8909
+
+# Architecture
 TARGET_ARCH := arm
-endif
-#add by wangtianyu for odex SW00187384 20150425 begin
+TARGET_CPU_ABI  := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a7
+TARGET_ARCH_VARIANT := armv7-a-neon
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := msm8909,x103f
+
+# Kernel
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk androidboot.selinux=permissive
+TARGET_KERNEL_SOURCE := kernel/lenovo/msm8909
+TARGET_KERNEL_CONFIG := msm8909_ar650x_defconfig
+BOARD_KERNEL_BASE        := 0x80000000
+BOARD_KERNEL_PAGESIZE    := 2048
+BOARD_RAMDISK_OFFSET     := 0x02000000
+BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(PWD)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
+BOARD_KERNEL_SEPARATED_DT := true
+TARGET_USES_UNCOMPRESSED_KERNEL := false
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+
+# Audio
+BOARD_USES_ALSA_AUDIO := true
+BOARD_SUPPORTS_SOUND_TRIGGER := true
+#AUDIO_FEATURE_ENABLED_DS2_DOLBY_DAP := true
+AUDIO_FEATURE_ENABLED_FM_POWER_OPT := true
+TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+
+# Camera
+USE_CAMERA_STUB := true
+TARGET_HAS_LEGACY_CAMERA_HAL1 := true
+
+# Dexpreopt
 ifeq ($(HOST_OS),linux)
   ifeq ($(WITH_DEXPREOPT),)
     ifeq ($(TARGET_BUILD_VARIANT),user)
@@ -31,78 +66,40 @@ ifeq ($(HOST_OS),linux)
     endif
   endif
 endif
-#add by wangtianyu for odex SW00187384 20150425 end
 
-BLOCK_BASED_OTA := false
-TARGET_OTA_ASSERT_DEVICE := msm8909,x103f
+# Memory Config
+MALLOC_SVELTE := true
 
-TARGET_COMPILE_WITH_MSM_KERNEL := true
-TARGET_KERNEL_APPEND_DTB := true
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(PWD)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
+# Encryption
+TARGET_HW_DISK_ENCRYPTION := true
 
-#AUDIO
-BOARD_USES_ALSA_AUDIO := true
-BOARD_SUPPORTS_SOUND_TRIGGER := true
-#AUDIO_FEATURE_ENABLED_DS2_DOLBY_DAP := true
-AUDIO_FEATURE_ENABLED_FM_POWER_OPT := true
-TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01000000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01000000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2684354560
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 11811160064
+BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 33554432
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_PERSISTIMAGE_FILE_SYSTEM_TYPE := ext4
 
-#FM Radio
+# FM Radio
 TARGET_QCOM_NO_FM_FIRMWARE := true
 BOARD_HAVE_QCOM_FM := true
 
-# Camera
-USE_CAMERA_STUB := true
-TARGET_HAS_LEGACY_CAMERA_HAL1 := true
-
--include $(QCPATH)/common/msm8909/BoardConfigVendor.mk
-TARGET_COMPILE_WITH_MSM_KERNEL := true
-
-# set the cryptfs_hw directory path
-TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/cryptfs_hw
-
-#TODO: Fix-me: Setting TARGET_HAVE_HDMI_OUT to false
-# to get rid of compilation error.
+# Graphics
 TARGET_HAVE_HDMI_OUT := false
 TARGET_USES_OVERLAY := true
 TARGET_USES_PCI_RCS := true
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
-TARGET_NO_BOOTLOADER := true
-TARGET_NO_KERNEL := false
-TARGET_NO_RADIOIMAGE := true
-TARGET_NO_RPC := true
 GET_FRAMEBUFFER_FORMAT_FROM_HWC := false
-
-BOOTLOADER_GCC_VERSION := arm-eabi-4.8
-BOOTLOADER_PLATFORM := msm8909# use msm8952 LK configuration
-
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_CPU_ABI  := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := cortex-a7
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_SMP := true
-ARCH_ARM_HAVE_TLS_REGISTER := true
-
+TARGET_USES_ION := true
+TARGET_USES_NEW_ION_API :=true
+TARGET_USES_QCOM_BSP := true
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_HARDWARE_3D := false
-TARGET_BOARD_PLATFORM := msm8909
-TARGET_BOOTLOADER_BOARD_NAME := msm8909
-
-TARGET_KERNEL_SOURCE := kernel/lenovo/msm8909
-TARGET_KERNEL_CONFIG := msm8909_ar650x_defconfig
-
-BOARD_KERNEL_BASE        := 0x80000000
-BOARD_KERNEL_PAGESIZE    := 2048
-BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
-BOARD_RAMDISK_OFFSET     := 0x02000000
-
-TARGET_USES_UNCOMPRESSED_KERNEL := false
-
-# Support to build images for 2K NAND page
-BOARD_KERNEL_2KPAGESIZE := 2048
-BOARD_KERNEL_2KSPARESIZE := 64
 
 # Shader cache config options
 # Maximum size of the  GLES Shaders that can be cached for reuse.
@@ -114,75 +111,40 @@ MAX_EGL_CACHE_KEY_SIZE := 12*1024
 # of the device.
 MAX_EGL_CACHE_SIZE := 2048*1024
 
-# Use signed boot and recovery image
-#TARGET_BOOTIMG_SIGNED := true
-
-TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_PERSISTIMAGE_FILE_SYSTEM_TYPE := ext4
-
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk androidboot.selinux=permissive
-BOARD_KERNEL_SEPARATED_DT := true
-
 BOARD_EGL_CFG := $(DEVICE_PATH)/configs/egl.cfg
 
-# Memory Config
-MALLOC_SVELTE := true
+# set the cryptfs_hw directory path
+TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/cryptfs_hw
 
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01000000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01000000
-# Modify by yanwenlong to partition size. (A6500M) SW00187112 2016-4-20 begin
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2684354560
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 11811160064
-# Modify by yanwenlong to partition size. (A6500M) SW00187112 2016-4-20 end
-BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
-BOARD_PERSISTIMAGE_PARTITION_SIZE := 33554432
-BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+# GPS
+TARGET_NO_RPC := true
 
-
-# Add NON-HLOS files for ota upgrade
-ADD_RADIO_FILES ?= true
-
-# Added to indicate that protobuf-c is supported in this build
+# Added to indicate that protobuf-c is not supported in this build
 PROTOBUF_SUPPORTED := false
 
-TARGET_USES_ION := true
-TARGET_USES_NEW_ION_API :=true
-TARGET_USES_QCOM_BSP := true
-
+# Init
 TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_msm
-#TARGET_INIT_VENDOR_LIB := libinit_msm
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 
-#Add support for firmare upgrade on msm8909
-HAVE_SYNAPTICS_I2C_RMI4_FW_UPGRADE := true
-
-TARGET_LDPRELOAD := libNimsWrap.so
-
-#Enable peripheral manager
+# Enable peripheral manager
 TARGET_PER_MGR_ENABLED := true
-
-#Enable HW based full disk encryption
-TARGET_HW_DISK_ENCRYPTION := true
 
 # Enable sensor multi HAL
 USE_SENSOR_MULTI_HAL := true
 
+# Ril
 FEATURE_QCRIL_UIM_SAP_SERVER_MODE := true
 
 # Control flag between KM versions
 TARGET_HW_KEYMASTER_V03 := true
 
-#BOARD_HAL_STATIC_LIBRARIES := libhealthd.msm
-
-#Add by tansen for Widevine fuction 20160408 SW00186672 begin
+# Widevine
 BOARD_WIDEVINE_OEMCRYPTO_LEVEL := 3
-#Add by tansen for Widevine fuction 20160408 SW00186672 end
 
 # SELinux policies
 include device/qcom/sepolicy/sepolicy.mk
 
-#Wifi
+# Wifi
 BOARD_WLAN_DEVICE                := qcwcn
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
